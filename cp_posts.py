065bc9df84cab3bacfb8b972d661815a66f46eed
lixiaolong <lixiaolong@sensoro.com>
2018-11-27 12:08:16 +0800

    update _posts/

--- cp_posts.py
+++ cp_posts.py
@@ -4,7 +4,9 @@ import os
 
 source_path = "/Users/sensoro/hexoBlog/source/_posts/*"
 base_path = "/Users/sensoro/hexoBlog/github_hexo/_posts/"
+os.system("rm -rf {0}".format(base_path))
 os.system("cp -r {0} {1}".format(source_path, base_path))
 os.system("git add . ")
 os.system("git commit -m 'update _posts/' ")
 os.system("git push origin master")
+
